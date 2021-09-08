386b3e6137ab7d64ee921cc0f7bca27748e8a752
boburwebdev <boburjon.zokirov.2017@gmail.com>
2021-09-08 21:36:41 +0500

    added solution with currentTarget

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -1,7 +1,15 @@
 const panelItems = document.querySelectorAll('.panel');
 
+// panelItems.forEach(panel => {
+//     panel.addEventListener('click', e => {
+//         e.target.closest('.panel').classList.toggle('open-active')
+//     })
+// });
+
+
+// Alternative solution with currentTarget
 panelItems.forEach(panel => {
     panel.addEventListener('click', e => {
-        e.target.closest('.panel').classList.toggle('open-active')
+        e.currentTarget.classList.toggle('open-active');
     })
-});
+})
